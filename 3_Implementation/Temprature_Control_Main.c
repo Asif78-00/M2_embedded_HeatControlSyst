4539801c9a2c77a48d4d38a3bc8ccfa2bc809c42
Asif78-00 <94295790+Asif78-00@users.noreply.github.com>
2021-12-01 23:51:01 +0530

    Update Temprature_Control_Main.c

--- 3_Implementation/Temprature_Control_Main.c
+++ 3_Implementation/Temprature_Control_Main.c
@@ -1,85 +1,31 @@
-/**
- * @file HCS_Main.c
- * @author MohammadAsif (md435610@gmail.com)
- * @brief The main program to execute the HCS of a seat inside a car
- * @version 0.1
- * @date 2021-11-30
- * 
- * @copyright Copyright (c) 2021
- * 
+/*
  */
+#include"Activity1.h"
+#include"Activity2.h"
+#include"Activity3.h"
+#include"Display.h"
 #include <avr/io.h>
-#include <util/delay.h>
-#include "ButtonSensorHeaterLED.h"
-#include "TemperatureSensorADC.h"
-#include "ADC_PWMoutput.h"
-#include "SerialCommunicationUART.h"
 
-
-/**
- * @brief A structure for the variables used inside the main function 
- * 
- */
-typedef struct{
-
-    uint16_t temp;
-    const char* tempvalue;
-
-}variables;
-
-variables var;
-
-/**
- * @brief Main function
- * 
- * @return int 
- */
 int main(void)
 {
-    Buttons_LED_Init(); //Initialise the button input registers
-    Init_ADC(); //Initialise the ADC
-    TimerWaveGenMode(); //Initialise the registers for TIMER1 as fast PWM
-    UARTinit(103); //Initialise UART registers
-    
-    int i;
 
-    /**
-     * @brief Infinite loop to run the microcontroller
-     * 
-     */
+    uint16_t temp;
     while(1)
     {
-       if(BUTTON_SENSOR_ON) //Checking if the input bit to 7th bit of pinB is made 0 from 1 by pressing led
+        if(Activity1_LED()==1)
         {
 
-            if(HEATER_ON) //Checking if the input bit to 6th bit of pinB is made 0 from 1 by pressing led
-            {
-                _delay_ms(20);
-                SET_LED; //make 0th bit of port B as 1, makes led glow
-                var.temp = Read_ADC(0);
-                var.tempvalue = outputbyPWM(var.temp);
-                for(i=0;i<=5;i++){
-                UARTwritecharacter(var.tempvalue[i]);
-
-                }
-                _delay_ms(20);
-                
-            }
-            else
-            {
-                _delay_ms(20);
-                OCR1A = 0; //make PWM output 0 if switch is off
-                CLEAR_LED; // make led off
-            }
+            TurnLED_ON();//Turn LED ON
+            temp=activity2_GetADC(); //Get the ADC value
+            activity3_PWM(temp); //PWM output based on temperature
+		    activity4_USARTWrite(temp); //To Serial monitor to print Temperature
         }
         else
         {
-                
-                CLEAR_LED; //make led off
-                OCR1A = 0; //make PWM output 0 if both switches are off
-                _delay_ms(20);
+            TurnLED_OFF();
+            _delay_ms(200);
         }
     }
 
- return 0;   
+    return 0;
 }

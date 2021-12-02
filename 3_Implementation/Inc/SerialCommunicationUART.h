84c5cdca23607614d9defdc92b3526ec54990370
Asif78-00 <94295790+Asif78-00@users.noreply.github.com>
2021-12-02 19:51:47 +0530

    Update SerialCommunicationUART.h

--- 3_Implementation/Inc/SerialCommunicationUART.h
+++ 3_Implementation/Inc/SerialCommunicationUART.h
@@ -1,6 +1,6 @@
 /**
  * @file SerialCommunicationUART.h
- * @author Hemanth A (hemanth.ec17@bmsce.ac.in)
+ * @author Mohammad Asif  (md435610@gmail.com)
  * @brief A header file for serial communication based on UART protocol
  * @version 0.1
  * @date 2021-04-29
